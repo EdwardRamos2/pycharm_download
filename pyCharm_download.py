4e3f567fb6af7effb5ceb9c13d635ca8be1dea90
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-12-08 20:03:17 -0500

    Update pyCharm_download.py

--- pyCharm_download.py
+++ pyCharm_download.py
@@ -11,7 +11,6 @@ def download_arquivos():
     print('[3] Install')
     opcao = input('option: >> ')
     if opcao == '1':
-        #import urllib.request
         url = 'https://dw.uptodown.com/dwn/35g50nV9UabTpPqNoxNPJlyKU8arD8JpU5Jssi0050Hr5gSWvfEFjkA65gfidf3xPHek0ARgJqbMkocbW57db4mI9tOAaHsDlUVBs6-Q65ga17Gveaazj1zXgcYWt5dr/51Lfyh0QdjYqUeXRphT_b67E7mpylCowxoy4BNl2pG5iwcEGGgcOeUmD7yCNJZRMmjDhc_7yPtro9CL5qAMiKJ7A1lK5tj0v1a7JdGb_XHf3OooDl7Cesk-LUa3hzu5m/HOWK4RVmJxaqsXzJUg9rLQr3WSMK5BLA_9CCot-nLsd9D7twkI73GuTntziptr6w4oov5hUHMp-NB9TUWhZBY7KxaGwwLJyLRhRJ-Al2ZlP0HkHdMnJb5MzYJHFeII_b/_zaEUkcnk4byxEcqvGkke8SqPGxUwM8-83x6-bB6altFrX7ysXYJzK3_Bu7gkbtkKLPF0kbLOSPmqRUFiPTIvw==/'
         file_name = 'universalUSB22.exe'
         # Download the file from `url` and save it locally under `file_name`:
@@ -19,7 +18,6 @@ def download_arquivos():
             data = response.read() # a `bytes` object
             out_file.write(data)
     if opcao == '2':
-        #import urllib.request
         url = input('Insira a URL para realizar o DOWNLOAD: \n')
         file_name = input('Insira o NOME do ARQUIVO seguido da extensao! \n')
         # Download the file from `url` and save it locally under `file_name`:
